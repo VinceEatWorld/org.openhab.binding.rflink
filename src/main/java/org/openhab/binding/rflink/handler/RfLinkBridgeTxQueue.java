6fd5b67b4ba6256fda43aa551161195b72673be4
cartemere <cartemere@online.fr>
2019-07-05 00:00:55 +0200

    [CODE] add headers for code quality checks

--- src/main/java/org/openhab/binding/rflink/handler/RfLinkBridgeTxQueue.java
+++ src/main/java/org/openhab/binding/rflink/handler/RfLinkBridgeTxQueue.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2010-2018 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.openhab.binding.rflink.handler;
 
 import java.io.IOException;
@@ -9,6 +17,11 @@ import org.openhab.binding.rflink.packet.RfLinkPacket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ *
+ * @author cartemere - Initial Contribution : externalize Rx/Tx from Bridge
+ *
+ */
 public class RfLinkBridgeTxQueue {
 
     private Logger logger = LoggerFactory.getLogger(RfLinkBridgeTxQueue.class);
